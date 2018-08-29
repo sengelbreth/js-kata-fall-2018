0d1c7689a3507287718b209c2fbadb19a176b041
sophie <sophie.engel1202@gmail.com>
2018-08-29 17:34:16 +0200

    elementer tilføjet

---
+++ w1d2 - this site uses cookies/mysolution/main.js
@@ -0,0 +1,7 @@
+    
+    document.getElementById("OK").addEventListener("click", myFunction);
+    
+    
+    function myFunction() {
+        document.getElementById("OK").innerHTML = "YOU CLICKED ME!";
+    }
